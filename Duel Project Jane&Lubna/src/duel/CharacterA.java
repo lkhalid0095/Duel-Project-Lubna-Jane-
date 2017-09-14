02a1d821362405d1da23c8216af694f3357bf797
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2017-09-14 11:03:32 -0400

    Created CharacterA

---
+++ Duel Project Jane&Lubna/src/duel/CharacterA.java
@@ -0,0 +1,33 @@
+package duel;
+
+public class CharacterA implements Dueler{
+
+	public CharacterA(){
+		//In Java, fields are instantiated in a constructor
+	
+	}
+	
+	public void taunt() {
+		
+	}
+
+	public String getName() {
+		
+		return null;
+	}
+
+	public void setStartingHP(int hp) {
+		
+		
+	}
+
+	public int getHP() {
+		
+		return 0;
+	}
+
+	public boolean determineIfOpponentIsFair(Dueler d, int hp) {
+
+		return false;
+	}
+}	
--- Duel Project Jane&Lubna/src/duel/Duel.java
+++ Duel Project Jane&Lubna/src/duel/Duel.java
@@ -24,9 +24,34 @@ public class Duel {
 	}
 	
 	public void dukeItOut(){
+		System.out.print(d1.getName());
+		pause(600);
+		System.out.print(" vs ");
+		pause(600);
+		System.out.println(d2.getName());
+		pause(600);
+		d1.taunt();
+		pause(2000);
+		d2.taunt();
+		pause(2000);
+		d1.setStartingHP(dueler1HP);
+		d2.setStartingHP(dueler2HP);
 		
 	}
 	
+	/**
+	 * This method involves "Threads" which we will get to much later in the year, essentially, it
+	 * creates a pause in the execution of the program, which makes it more dramatic
+	 * @param time
+	 */
+	public void pause(long time){
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
 
 }
 
